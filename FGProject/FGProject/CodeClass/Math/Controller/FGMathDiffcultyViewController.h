23f3aa4f1b88c4b3e9892bfb75acc267bacfc452
鑫龙魂 <1305655563@qq.com>
2018-08-26 16:51:01 +0800

    添加困难模式

---
+++ FGProject/FGProject/CodeClass/Math/Controller/FGMathDiffcultyViewController.h
@@ -0,0 +1,13 @@
+//
+//  FGMathDiffcultyViewController.h
+//  FGProject
+//
+//  Created by 鑫龙魂 on 2018/8/26.
+//  Copyright © 2018年 bert. All rights reserved.
+//
+
+#import "FGMathBaseViewController.h"
+
+@interface FGMathDiffcultyViewController : FGMathBaseViewController
+
+@end
---
+++ FGProject/FGProject/CodeClass/Math/Controller/FGMathDiffcultyViewController.m
@@ -0,0 +1,39 @@
+//
+//  FGMathDiffcultyViewController.m
+//  FGProject
+//
+//  Created by 鑫龙魂 on 2018/8/26.
+//  Copyright © 2018年 bert. All rights reserved.
+//
+
+#import "FGMathDiffcultyViewController.h"
+
+@interface FGMathDiffcultyViewController ()
+
+@end
+
+@implementation FGMathDiffcultyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)initSubviews{
+    [super initSubviews];
+    self.titleStr = @"困难模式";
+    
+    
+}
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
